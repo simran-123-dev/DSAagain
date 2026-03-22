107029641969e70b2a82ad138d7ed2b3e50db7b9
simran-123-dev <kaursimran192007@gmail.com>
2026-03-22 09:43:08 +0530

    Create MaxSubarray.cpp

---
+++ Arrays/MaxSubarray.cpp
@@ -0,0 +1,114 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum=0;
+        int maxSum=INT_MIN;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            currSum=currSum+nums[i];
+            maxSum=max(maxSum,currSum);
+            if(currSum<0){
+                currSum=0;
+            }
+        }
+        return maxSum;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
